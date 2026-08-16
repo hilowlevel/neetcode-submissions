1bbb4e897682326c0d20b666758a974544d70652
hilowlevel <123559121+hilowlevel@users.noreply.github.com>
2026-08-16 15:21:58 +0900

    Add: trapping-rain-water - submission-0

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        vector<int> water_left;
+        vector<int> water_right;
+        int answer = 0;
+
+        int left_max = left;
+        int right_max = right;
+
+        for(int i = 0; i < height.size() - 1; i++){
+            if(height[i] > height[left_max]) left_max = i;
+            water_left.push_back(height[left_max]);
+        }
+        for(int i = 0; i <= height.size() - 1; i++){
+            if(height[height.size() - i - 1] > height[right_max]) right_max = height.size() - i - 1;
+            water_right.insert(water_right.begin(), height[right_max]);
+        }
+        for(int i = 0; i < height.size() - 1; i++){
+            answer += (min(water_left[i], water_right[i]) - height[i]);
+        }
+            
+        return answer;
+    }
+};
