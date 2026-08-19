9dd3943acc9c9547aff999a16590f7cad3befb7a
hilowlevel <123559121+hilowlevel@users.noreply.github.com>
2026-08-19 23:02:28 +0900

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.cpp
@@ -0,0 +1,24 @@
+#include <unordered_set>
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int ans = 0;
+        int left = 0;
+        int right = 0;
+        unordered_set<char> temp;
+        for(int i = 0; i < s.size(); i++){
+            if(temp.find(s[i]) == temp.end()) temp.insert(s[i]);
+            else{
+                while(temp.find(s[i]) != temp.end()){
+                    left++;
+                    temp.erase(s[left - 1]);
+                }
+                temp.insert(s[i]);
+            }
+            if(ans < right - left + 1) ans = right - left + 1;
+            right++;
+        }
+        return ans;
+    }
+};
