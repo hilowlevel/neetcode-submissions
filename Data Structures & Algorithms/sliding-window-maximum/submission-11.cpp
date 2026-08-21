6f32052e63c5167b3ec3aed33622b2af4e5a56ee
hilowlevel <123559121+hilowlevel@users.noreply.github.com>
2026-08-21 23:06:01 +0900

    Add: sliding-window-maximum - submission-11

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-11.cpp
@@ -0,0 +1,26 @@
+#include <queue>
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int left = 1;
+        int right = k;
+        vector<int> ans;
+        std::priority_queue<std::pair<int, int>> heap;
+        
+        for(int i = 0; i < k; i++){
+            heap.push({nums[i], i});
+        }
+        ans.push_back(heap.top().first);
+
+        while(right < nums.size()){
+            while(heap.size() != 0 && left - 1 >= heap.top().second) heap.pop();
+            heap.push({nums[right], right});
+            ans.push_back(heap.top().first);
+            left++;
+            right++;
+        }
+
+        return ans;
+    }
+};
